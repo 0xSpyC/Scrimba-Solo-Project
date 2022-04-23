0896299f3c8ce31a7c725de5cc49f34b2f4641eb
Reda Tazlaoui <rtazlaou@k0r1p9.42mulhouse.fr>
2022-04-23 12:01:22 +0200

    CSS Styling Info components

--- Project1/src/components/Info.js
+++ Project1/src/components/Info.js
@@ -5,16 +5,15 @@ import LinkedIn from '../images/LinkedIn.png';
 
 export default function Info() {
     return (
-        <div>
-            <div>
-                <img src="https://i.pinimg.com/236x/2c/29/06/2c2906f381cdc5b8595bd140dfb7bd01.jpg" className="profile--picture"/>
+        <div className="info">
+            <div className="info--profile">
             </div>
-            <div>
-                <h1>Porsche</h1>
-                <h2>Automobile Manufacturer</h2>
-                <h3>porsche.com</h3>
+            <div className="info--details">
+                <h1 className="details--title">Porsche</h1>
+                <h2 className="details--job">Automobile Manufacturer</h2>
+                <h3 className="details--web">porsche.com</h3>
             </div>
-            <div>
+            <div className="info--button">
                 <button><img src={Email} className="button"/></button>                
                 <button><img src={LinkedIn} className="button"/></button>
             </div>
