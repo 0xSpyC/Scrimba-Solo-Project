cf75f00bf7a657cc7e7af098547f388ca9716e7b
Reda Tazlaoui <rtazlaou@k0r1p9.42mulhouse.fr>
2022-04-23 14:00:45 +0200

    CSS Styling Main components

--- Project1/src/components/About.js
+++ Project1/src/components/About.js
@@ -3,8 +3,8 @@ import React from "react"
 export default function Description() {
     return (
         <div>
-            <h2>About</h2>
-            <h3>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</h3>
+            <h2 className="main--title">About</h2>
+            <h3 className="main--text">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</h3>
         </div>
     )
 }
